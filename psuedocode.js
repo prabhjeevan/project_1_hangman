171261eb45cb6c66312b6ad88f89f2f62b98a90d
Prabhjeevan Kalra <prabhjeevan@MacBook-Pro.local>
2020-10-13 16:55:13 -0400

    psuedocode and wireframe

---
+++ psuedocode.js
@@ -0,0 +1,36 @@
+// Show intro page with start button 
+// show next page with prompt to pick a category 
+// generate a word at random from the array of that category 
+// and store that word in some variable
+// display empty blanks to the user with the same length of the random word that was generated
+// prompt the use the guess a letter 
+
+// if letter matches a character from the selected word show on screen and increment correct guess +1
+// check if correct guess increment equals length of random generated word
+// if not equal to length
+// prompt the user to guess a letter
+// if it is equal to length 
+// show "you guessed it" 
+// prompt user to play again or exit
+
+
+// if the guess is incorrect
+// put the letter in the wrong letter box 
+// show the head generate and increment incorrect +1
+// check if the incorrect guess increment equals 7
+// if it is 8 --> show gameover you lose 
+// prompt user to play again or exit
+// if less than 8 then prompt user to guess again 
+
+// repeat line 8 - 23
+
+// BREAKDOWN OF THE INCORRECT GUESSES AND WHAT WILL BE THE RESULT OF IT  
+// if incorrect guess increment = 
+// 1 = head
+// 2 = body 
+// 3 = right arm
+// 4 = eft arm 
+// 5 = right leg
+// 6 = left leg
+//7 = eyes
+
